ed2d04f522bdbd30adc7734c9a649f0648ea99ae
bobans@ <bobans@inbox.ru>
2016-05-18 15:49:22 +0300

    commit from job end job for login form

--- web/auth.php
+++ web/auth.php
@@ -1,29 +1,10 @@
 <?php
-// Данный файл всегда будит "включаться" в другие файлы
-// директивой include поэтому следует запретить его самостоятельный вызов
-// из строки запроса путём указания его имени
-// Если не определена константа IN_ADMIN - завершаем работу скрипта
 require_once __DIR__ . '/library/autoload.php';
-
 if(!defined("IN_ADMIN")) die;
 
-//$login = "login";
-//$passw = "password";
-$login = "1";
-$passw = "1";
-
-// Проверям были ли посланы данные
-/*echo "session:";
-echo $_POST['enter'];
-echo $_SESSION['login'];
-echo $_SESSION['passw'];
-echo $_SESSION['agent'];
-echo $_SESSION['ip'];
-
-echo $_POST['login'];
-echo $_POST['passw'];
-echo $_SERVER['HTTP_USER_AGENT'];
-echo $_SERVER['REMOTE_ADDR'];*/
+$login = "login";
+$passw = "password";
+$WrongPassword = "Введите логин и пароль необходимы для входа в систему";
 
 if(!empty($_POST['enter']))
 {
@@ -39,7 +20,7 @@ if ((!empty($_POST['enter'])) and
     (empty($_SESSION['login']) or
     md5($login) != $_SESSION['login'] or
     md5($passw) != $_SESSION['passw'] )
-) echo('<div class="col-sm-6 col-md-4 col-md-offset-4 text-danger">Неверный логин либо пароль!</div>');
+) $WrongPassword='<p class="text-danger">Неверный логин либо пароль!</p>';
 
 if(empty($_SESSION['login']) or
     md5($login) != $_SESSION['login'] or
@@ -52,11 +33,13 @@ if(empty($_SESSION['login']) or
             <div class="container">
                 <div class="row">
                     <div class="Absolute-Center is-Responsive">
-                        <div class="col-sm-6 col-md-4 col-md-offset-4">
+                        <div class="form-group text-center" id = "WrongPassword">'.$WrongPassword.'
+                        </div>
+                        <div class="col-sm-12 col-md-10 col-md-offset-1">
                             <form name="form_auth" method="post" action="" id="loginForm">
                                 <div class="form-group input-group">
                                     <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                    <input width="100px" class="form-control" type="text" name="login" placeholder="username"/>
+                                    <input  class="form-control" type="text" name="login" placeholder="username"/>
                                  </div>
                                 <div class="form-group input-group">
                                     <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
@@ -71,6 +54,10 @@ if(empty($_SESSION['login']) or
                         </div>
                     </div>
                 </div>
+                <!--футер----->
+            </div>
+            </body>
+            </html>
         ');
     die;
-    }
+    } else define("LOGIN", TRUE);
--- web/kia.php
+++ web/kia.php
@@ -21,49 +21,50 @@
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="bootstrap/js/bootbox.min.js "></script>
 
+	<style>
+		.Absolute-Center {
+			margin: auto;
+			position: absolute;
+			top: 0; left: 0; bottom: 0; right: 0;
+		}
 
-
+		.Absolute-Center.is-Responsive {
+			width: 50%;
+			height: 50%;
+			min-width: 400px;
+			max-width: 600px;
+			padding: 40px;
+		}
+	</style>
+	<script>
+		$(function(){
+			$("#SatellitesCount").click(function(){
+				$.post("kia.php?step=Logout",function(data){
+					window.location.href="kia.php";
+				});
+			});
+		});
+	</script>
 
 </head>
 <body role="document">
-
-<form method="POST" action="kia.php?step=Run" id="Run">
-<input type="hidden" name="flag" value="true" />
-</form>
-<form method="POST" action="kia.php?step=Stop" id="Stop">
-<input type="hidden" name="flag" value="true" />
-</form>
-
-<div class="navbar navbar-inverse" role="navigation">
-      <div class="container-fluid">
-	<a id="SatellitesCount" class="navbar-brand" href="#">X</a> 
-        <div class="navbar-header">
-          	  <a class="navbar-brand" href="kia.php">Status</a>
-<!--		  <a class="navbar-brand" href="kia.php?step=Run">Run</a>-->
-<!--		  <a class="navbar-brand" href="kia.php?step=Stop">Stop</a>-->
-		  <a class="navbar-brand" href="javascript:bootbox.confirm('Вы уверены, что нужно запустить двигатель?', function(result) { if (result) { document.getElementById('Run').submit();   } }	); ">Run</a>
-		  <a class="navbar-brand" href="javascript:bootbox.confirm('Вы уверены, что нужно заглушить двигатель?', function(result) { if (result) { document.getElementById('Stop').submit();   } }	); ">Stop</a>
-
-
-        </div>
-<!--        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-		  <li><a id="SatellitesCount" class="navbar-brand" href="kia.php?step=more"></a></li>
-          </ul>
-        </div> -->
-      </div>
-    </div>
-
-
-<div class="container-fluid"> 
-
+	<div class="navbar-fixed-bottom row-fluid">
+		<div class="navbar-inner">
+			<div class="container text-right">
+				<small>Kia Remoto bobans@ 2016</small>
+			</div>
+		</div>
+	</div>
+	<form method="POST" action="kia.php?step=Run" id="Run">
+		<input type="hidden" name="flag" value="true" />
+	</form>
+	<form method="POST" action="kia.php?step=Stop" id="Stop">
+		<input type="hidden" name="flag" value="true" />
+	</form>
 
 <?php
-
-//error_reporting(E_ALL);
-//ini_set('display_errors',1);
-//require_once __DIR__ . '/library/autoload.php';
-
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 //Авторизация
 define("IN_ADMIN", TRUE);
 require "auth.php";
@@ -71,58 +72,73 @@ require "auth.php";
 $login = 'phone';
 $password = 'password';
 
-
-if ($_GET["step"]=='Run') {
-     if ($_POST["flag"]=='true') {
-	echo("<p> Run engine car:</p>");	
-	$postData=  '{"Info":{"CommandType":100}}';
-	
-	$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
-	echo('<details><summary>Log read status</summary>' . prettyPrint($http) . '</details>');
-	$res = json_decode($http);
-	if ($res==null) die('Could not connect to remote server!');
-	if ($res->{'Network'}->{'Status'}==0) 	echo('<script>$( "#SatellitesCount" ).text( "'.$res->{'Network'}->{'SatellitesCount'}.' | " ); </script> ');
-	if ( (!$res->{'Engine'}->{'IsOn'}) and ($res->{'Engine'}->{'Rpm'}==0) ) {
-		$http = http_request_kia('https://rmt.brightbox.ru/api/v1/devices/commands',$postData,$login,$password,TRUE);
-		echo('<details><summary>Log after run engine</summary>' . prettyPrint($http) . '</details>');
-		if (strlen($http)>0) {
-			sleep(10);
-			$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
-			echo('<details><summary>Log after run engine / status</summary>' . prettyPrint($http) . '</details>');
-			$res = json_decode($http);
-			echo('Last message from car: '.$res->{'Info'}->{'Message'});
-		}
-	} else echo('Двигатель запущен,мы не можем его запустить');
-    } else echo('Неправильный переход на страницу... Переходим на главную <script> window.location.href="kia.php";</script>');
+if (LOGIN<>null) {
+	echo('
+	<div class="navbar navbar-inverse" role = "navigation" >
+      <div class="container-fluid" >
+		<a id = "SatellitesCount" class="navbar-brand" href = "#" > X</a >
+        <div class="navbar-header" >
+        	<a class="navbar-brand" href = "kia.php" > Status</a >
+			<a class="navbar-brand" href = "javascript:bootbox.confirm(\'Вы уверены, что нужно запустить двигатель?\', function(result) { if (result) { document.getElementById(\'Run\').submit();   } }	); " > Run</a >
+		  	<a class="navbar-brand" href = "javascript:bootbox.confirm(\'Вы уверены, что нужно заглушить двигатель?\', function(result) { if (result) { document.getElementById(\'Stop\').submit();   } }	); " > Stop</a >
+        </div >
+      </div >
+    </div >
+    ');
 }
 
-if ($_GET["step"]=='Stop') {
+echo('<div class="container-fluid">');
+if ((isset($_GET["step"])) and ($_GET["step"]=='Logout')) {	destroySession(); }
+if ((isset($_GET["step"])) and ($_GET["step"]=='Run')) {
+    if ($_POST["flag"]=='true') {
+		echo("<p> Run engine car:</p>");
+		$postData=  '{"Info":{"CommandType":100}}';
+		$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
+		echo('<details><summary>Log read status</summary>' . prettyPrint($http) . '</details>');
+		$res = json_decode($http);
+		if ($res==null) die('Could not connect to remote server!');
+		if ($res->{'Network'}->{'Status'}==0) 	echo('<script>$( "#SatellitesCount" ).text( "'.$res->{'Network'}->{'SatellitesCount'}.' | " ); </script> ');
+		if ( (!$res->{'Engine'}->{'IsOn'}) and ($res->{'Engine'}->{'Rpm'}==0) ) {
+			$http = http_request_kia('https://rmt.brightbox.ru/api/v1/devices/commands',$postData,$login,$password,TRUE);
+			echo('<details><summary>Log after run engine</summary>' . prettyPrint($http) . '</details>');
+			if (strlen($http)>0) {
+				sleep(10);
+				$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
+				echo('<details><summary>Log after run engine / status</summary>' . prettyPrint($http) . '</details>');
+				$res = json_decode($http);
+				echo('Last message from car: '.$res->{'Info'}->{'Message'});
+			}
+		} else echo('Двигатель запущен,мы не можем его запустить');
+    	} else echo('Неправильный переход на страницу... Переходим на главную <script> window.location.href="kia.php";</script>');
+}
+
+if ((isset($_GET["step"])) and ($_GET["step"]=='Stop')) {
      if ($_POST["flag"]=='true') {
-	$postData=  '{"Info":{"CommandType":102}}';
-	$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
-	echo('<details><summary>Log read status</summary>' . prettyPrint($http) . '</details>');
-	$res = json_decode($http);
-	if ($res==null) die('Could not connect to remote server!');
-	if ($res->{'Network'}->{'Status'}==0) 	echo('<script>$( "#SatellitesCount" ).text( "'.$res->{'Network'}->{'SatellitesCount'}.' | " ); </script> ');
-	if ( ($res->{'Engine'}->{'IsOn'}) and ($res->{'Engine'}->{'Rpm'}>0) ) {
-		$http = http_request_kia('https://rmt.brightbox.ru/api/v1/devices/commands',$postData,$login,$password,TRUE);
-		echo('<details><summary>Log of stop engine</summary>' . prettyPrint($http) . '</details>');
-		if (strlen($http)>0) {
-			sleep(10);
-			$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
-			echo('<details><summary>Log after stop engine / status</summary>' . prettyPrint($http) . '</details>');
-			$res = json_decode($http);
-			echo('Last message from car: '.$res->{'Info'}->{'Message'});
-		}
-	} else echo('Двигатель не запущен,мы не можем его оcтановить');
-    } else echo('Неправильный переход на страницу... Переходим на главную <script> window.location.href="kia.php";</script>');
+		$postData=  '{"Info":{"CommandType":102}}';
+		$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
+		echo('<details><summary>Log read status</summary>' . prettyPrint($http) . '</details>');
+		$res = json_decode($http);
+		if ($res==null) die('Could not connect to remote server!');
+		if ($res->{'Network'}->{'Status'}==0) 	echo('<script>$( "#SatellitesCount" ).text( "'.$res->{'Network'}->{'SatellitesCount'}.' | " ); </script> ');
+		if ( ($res->{'Engine'}->{'IsOn'}) and ($res->{'Engine'}->{'Rpm'}>0) ) {
+			$http = http_request_kia('https://rmt.brightbox.ru/api/v1/devices/commands',$postData,$login,$password,TRUE);
+			echo('<details><summary>Log of stop engine</summary>' . prettyPrint($http) . '</details>');
+			if (strlen($http)>0) {
+				sleep(10);
+				$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
+				echo('<details><summary>Log after stop engine / status</summary>' . prettyPrint($http) . '</details>');
+				$res = json_decode($http);
+				echo('Last message from car: '.$res->{'Info'}->{'Message'});
+			}
+		} else echo('Двигатель не запущен,мы не можем его оcтановить');
+	    } else echo('Неправильный переход на страницу... Переходим на главную <script> window.location.href="kia.php";</script>');
 }
 
 if ((!isset($_GET["step"])) or ($_GET["step"]=='Status')) {
-	echo("<h4> Status for car:</h4>");
 	$http = http_request_kia('https://rmt.brightbox.ru/api/v2/devices','',$login,$password);
 	$res = json_decode($http);
 	if ($res==null) die('Could not connect to remote server!');
+	echo("<h4> Status for car:</h4>");
 	$engine=$res->{'Engine'}->{'IsOn'};
 	$rpm=$res->{'Engine'}->{'Rpm'};
 	$dateofmessage=Date('d.m.Y H:i:s',substr($res->{'Info'}->{'Date'},6,10));
